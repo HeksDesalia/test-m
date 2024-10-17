1d3f6f77ff99695856ad01fb3afc209094d9bc6c
elystal <exelwtf@gmail.com>
2024-10-17 22:59:58 +0200

    bla

--- src/app/form-field-container/form-field-container.component.ts
+++ src/app/form-field-container/form-field-container.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-form-field-container',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule],
   templateUrl: './form-field-container.component.html',
   styleUrl: './form-field-container.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
